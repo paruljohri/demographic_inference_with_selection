81806653a2cb33a76a08e4f1ed256a603a603dcf
Parul Johri <48296846+paruljohri@users.noreply.github.com>
2020-12-19 19:59:12 -0500

    Add files via upload

--- OtherScripts/create_ms_from_diff_chromosomes.py
+++ OtherScripts/create_ms_from_diff_chromosomes.py
@@ -1,7 +1,7 @@
 #This is to create an ms file that has SNPs from different chromosomes
 #It is to meausre the baseline unlinked LD
 #How to run:
-#python create_ms_from_diff_chromosomes.py -regionLen 10000000 -chr_size 10Mb
+#python create_ms_from_diff_chromosomes.py -regionLen 10000000 -chr_size 10Mb -thinning all
 
 import sys
 import os
@@ -12,15 +12,20 @@ import random
 parser = argparse.ArgumentParser(description='Information about number of sliding windows and step size')
 parser.add_argument('-regionLen', dest = 'regionLen', action='store', nargs = 1, type = int, help = 'length in bp of region simulated')#Length of coding region simulated
 parser.add_argument('-chr_size', dest = 'chr_size', action='store', nargs = 1, type = str, help = 'foldername with .ms files')
-#parser.add_argument('-output_folder', dest = 'output_folder', action='store', nargs = 1, type = str, help = 'full path to folder where you want to write the output')
+parser.add_argument('-thinning', dest = 'thinning', action='store', nargs = 1, type = str, help = 'full path to folder where you want to write the output')
 #parser.add_argument('-output_prefix', dest = 'output_prefix', action='store', nargs = 1, type = str, help = 'full path to output file')
 args = parser.parse_args()
 chr_len =  args.regionLen[0]
 chr_size =  args.chr_size[0]
-#infolder = args.input_folder[0]
+thinning = args.thinning[0]
 #outfolder = args.output_folder[0]
 #prefix = args.output_prefix[0]
 num_indv = 100
+
+if chr_size == "1Gb":
+    num_reps = 10
+else:
+    num_reps = 100
 
 #read ms file:
 def read_subset_ms(f_ms, start, end):
@@ -37,7 +42,7 @@ def read_subset_ms(f_ms, start, end):
                     if (float(x) >= float(start)) and (float(x) <= float(end)):
                         l_Pos.append(float(x))
                         d_gt[str(i)] = ""
-                        l_int_posn.append(i)
+                        l_int_posn.append(i)#1,2...n
                     i = i + 1
         elif "//" not in line and "segsites" not in line:
             for j in l_int_posn:
@@ -53,36 +58,46 @@ while draw_num <= 100:
 
 #go through all simulation replicates and read data into pylibseq format
 repID = 1
-while repID <= 100:
+while repID <= num_reps:
     print ("Reading file:" + str(repID))
-    f_ms = open("/scratch/kriall/fsc/REDO_single_chr/chromosomes/" + chr_size + "/" + "output_single_chr_" + chr_size + "_rep" + str(repID) + ".ms", 'r') #for all SNPs
-    #f_ms = open("/scratch/kriall/fsc/REDO_single_chr/chromosomes/" + chr_size + "/" + "output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_5kb.ms", 'r') #for 1 in 5kb
-    #f_ms = open("/scratch/kriall/fsc/REDO_single_chr/chromosomes/" + chr_size + "/" + "output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_50kb.ms", 'r') #for 1 in 50kb
-    #f_ms = open("/scratch/kriall/fsc/REDO_single_chr/chromosomes/" + chr_size + "/" + "output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_100kb.ms", 'r') #for 1 in 100kb
+    if thinning == "all":
+        f_ms = open("/scratch/kriall/fsc/REDO_single_chr/chromosomes/" + chr_size + "/" + "output_single_chr_" + chr_size + "_rep" + str(repID) + ".ms", 'r') #for all SNPs
+    elif thinning == "5kb":
+        f_ms = open("/scratch/kriall/fsc/REDO_single_chr/chromosomes/" + chr_size + "/" + "output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_5kb.ms", 'r') #for 1 in 5kb
+    elif thinning == "50kb":
+        f_ms = open("/scratch/kriall/fsc/REDO_single_chr/chromosomes/" + chr_size + "/" + "output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_50kb.ms", 'r') #for 1 in 50kb
+    elif thinning == "100kb":    
+        f_ms = open("/scratch/kriall/fsc/REDO_single_chr/chromosomes/" + chr_size + "/" + "output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_100kb.ms", 'r') #for 1 in 100kb
 
     d_ms = read_subset_ms(f_ms, 0.0, 1.0)
     f_ms.close()
-    draw_num = 1
-    while draw_num <= 100:
-        s_rand = random.randint(0, len(d_ms)-1)
-        d_random_gts[draw_num].append(d_ms[str(s_rand)])
-        draw_num += 1
+    if len(d_ms["1"]) == num_indv:#checking that the .ms file has the number of individuals assumed
+        draw_num = 1
+        while draw_num <= 100:
+            s_rand = random.randint(1, len(d_ms))
+            d_random_gts[draw_num].append(d_ms[str(s_rand)])
+            draw_num += 1
     repID += 1
+print(len(d_random_gts))
 
 #write the 100 draws into .ms files with arbitrary positions
 draw_num = 1
-s_posn_interval = float(chr_len)/100.0
 while draw_num <= 100:
+    num_SNPs = len(d_random_gts[draw_num])
+    s_posn_interval = float(chr_len)/float(num_SNPs)
+    #print ("draw num: " + str(draw_num))
     result = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(draw_num) + ".ms", 'w+')
-    result.write("//" + '\n' + "segsites: 100" + '\n' + "positions:")
+    result.write("//" + '\n' + "segsites: " + str(num_SNPs) + '\n' + "positions:")
     j=1
-    while j<=100:
+    while j<=num_SNPs:
         result.write(" " + str((s_posn_interval*float(j))/float(chr_len)))
         j += 1
     #write the actual genotypes:
     row_num=0
     while row_num<num_indv:
         for l_gts in d_random_gts[draw_num]:
+            #print (len(l_gts))
+            #print ("row_num" + str(row_num))
             result.write(l_gts[row_num])
         result.write('\n')
         row_num += 1
--- OtherScripts/summarize_LD_stats.py
+++ OtherScripts/summarize_LD_stats.py
@@ -1,28 +1,39 @@
 #get rsq mean and sd
+#How to run:
+#python summarize_LD_stats.py 1Gb linked all
+#python summarize_LD_stats.py 1Gb unlinked ""
 import sys
 #import statistics
 import numpy
 
-chr_size = "200Mb"
+chr_size = sys.argv[1] #1Mb/10Mb/50Mb/200Mb/1Gb
+linkage_status = sys.argv[2] #unlinked/ linked
+thinning = sys.argv[3] #all/5kb/50kb/100kb
 
 l_rsq = []
 l_S = []
 repID = 1
 while repID <= 100:
-    #f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "/output_single_chr_" + chr_size + "_rep" + str(repID) + "_10000.stats", 'r')
-    #f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "/output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_5kb_50000.stats", 'r')
-    #f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "/output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_50kb_500000.stats", 'r')
-    f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "/output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_100kb_1000000.stats", 'r')
-    if chr_size=="1Mb":
-        f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_100000.stats", 'r')
-    elif chr_size=="10Mb":
-        f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_1000000.stats", 'r')
-    #elif chr_size=="50Mb":
-    #    f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_5000000.stats", 'r')
-    #elif chr_size=="200Mb":
-    #    f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_20000000.stats", 'r')
-    elif chr_size == "1Gb":
-        f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_100000000.stats", 'r')
+    if linkage_status == "linked":
+        if thinning == "all":
+            f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "/output_single_chr_" + chr_size + "_rep" + str(repID) + "_10000.stats", 'r')
+        elif thinning == "5kb":
+            f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "/output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_5kb_50000.stats", 'r')
+        elif thinning == "50kb":
+            f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "/output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_50kb_500000.stats", 'r')
+        elif thinning == "100kb":
+            f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "/output_single_chr_" + chr_size + "_rep" + str(repID) + "_thinned_100kb_1000000.stats", 'r')
+    elif linkage_status=="unlinked":
+        if chr_size=="1Mb":
+            f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_100000.stats", 'r')
+        elif chr_size=="10Mb":
+            f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_1000000.stats", 'r')
+        elif chr_size=="50Mb":
+            f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_5000000.stats", 'r')
+        elif chr_size=="200Mb":
+            f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_20000000.stats", 'r')
+        elif chr_size == "1Gb":
+            f_tab = open("/home/pjohri1/MSMC/thinned_SNPs_LD/" + chr_size + "_unlinked/draw" + str(repID) + "_100000000.stats", 'r')
     d_col = {}
     for line in f_tab:
         line1 = line.strip('\n')
